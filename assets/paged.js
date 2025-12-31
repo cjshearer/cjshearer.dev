6095cbea5d158e9bac857279d44d4682dd699238
cjshearer <cjshearer@live.com>
2025-12-31 16:20:36 -0500

    fix: don't rely on timing allowed by onbeforeprint

--- assets/paged.js
+++ assets/paged.js
@@ -1,31 +1,38 @@
 /* TODO: see caching behavior for GetRemote so we can fetch this during nix build  */
 import "https://unpkg.com/pagedjs@0.4.3/dist/paged.min.js";
 
-async function initializePagedJS() {
-  await new Promise((resolve) => {
-    if (document.readyState !== "loading") resolve();
-    document.addEventListener("DOMContentLoaded", () => resolve());
-  });
+const PRINT_PARAM = "print";
 
+function inPrintMode() {
+  return new URL(window.location.href).searchParams.has(PRINT_PARAM);
+}
+
+function toPrintUrl() {
+  const url = new URL(window.location.href);
+  url.searchParams.set(PRINT_PARAM, "true");
+  return url;
+}
+
+function toScreenUrl() {
+  const url = new URL(window.location.href);
+  url.searchParams.delete(PRINT_PARAM);
+  return url;
+}
+
+async function initializePagedJS() {
   const previewer = new PagedModule.Previewer();
   await previewer.preview();
 }
 
-async function showPrintPage() {
-  const url = new URL(window.location.href);
-  url.searchParams.set("print", "true");
-  history.pushState({}, "", url);
-  await initializePagedJS();
-}
-
-if (new URL(window.location.href).searchParams.has("print")) {
-  initializePagedJS();
-} else {
-  window.onbeforeprint = showPrintPage;
-}
-
 window.onafterprint = () => {
-  const url = new URL(window.location.href);
-  url.searchParams.delete("print");
-  window.location.href = url.href;
+  window.location.replace(toScreenUrl().href);
 };
+
+if (inPrintMode()) {
+  await initializePagedJS();
+  window.print();
+} else {
+  window.onbeforeprint = () => {
+    window.location.replace(toPrintUrl().href);
+  };
+}
